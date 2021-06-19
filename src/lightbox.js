d615102ba35d79abac4e4eb4ffdf74bcf171bd16
farvell <farvellsmurf@gmail.com>
2021-06-19 07:28:40 +0200

    refactorizing lightbox

--- src/lightbox.js
+++ src/lightbox.js
@@ -1,273 +1,269 @@
 // Lightbox logic and constructor
 export default class Lightbox {
 
-	// fetching data classes to control lightbox
-	constructor( config ) {
+    // fetching data classes to control lightbox
+    constructor( config ) {
         
-		// Initializing handler
-		this.handler = new LightboxHandler({
-			element: "lightbox",
-			css: config.css
-		},
-		{
-			element: "lightbox-close",
-			css: config.css
-		});
-		// Set param config
-		this._setLightboxConfig( config );
-		// Init roullette of images
-		this._initRoullette();
+        // Initializing handler
+        this.handler = new LightboxHandler({
+            element: "lightbox",
+            css: config.css
+        });
+        // Set param config
+        this._setLightboxConfig( config );
+        // Init roullette of images
+        this._initRoullette();
 
-		console.dir( this.config );
-		// Return listen lightbox
-		return this.listen();
-	}
+        console.dir( this.config );
+        // Return listen lightbox
+        return this.listen();
+    }
 
-	_setLightboxConfig( config ) {
-		this.config =  {
-			images: [ ...document.getElementsByClassName( config.images ) ],
-			texts: [ ...document.getElementsByClassName( config.texts ) ],
-			lightbox: {
-				photo: document.getElementById( "lighbox-photo" ),
-				caption: document.getElementById( "lightbox-caption" ),
-			},
-			roullette: {
-				img: document.getElementById( "lightbox-roullette" ),
-				txt: []
-			},
-			control: "lightbox-control",
-			exit: "lightbox-close",
-			conditions: {
-				roullette: "roullette-image",
-				previous: "previous-button", 
-				next: "next-button",
-				length: 2
-			}
-		};
-	}
+    _setLightboxConfig( config ) {
+        this.config =  {
+            images: [ ...document.getElementsByClassName( config.images ) ],
+            texts: [ ...document.getElementsByClassName( config.texts ) ],
+            lightbox: {
+                photo: document.getElementById( "lighbox-photo" ),
+                caption: document.getElementById( "lightbox-caption" ),
+            },
+            roullette: {
+                img: document.getElementById( "lightbox-roullette" ),
+                txt: []
+            },
+            control: "lightbox-control",
+            exit: "lightbox-close",
+            conditions: {
+                roullette: "roullette-image",
+                previous: "previous-button", 
+                next: "next-button",
+                length: 2
+            }
+        };
+    }
 
-	// initializing roullette from fetched images
-	_initRoullette() {
-		this.config.images.forEach(( img, index ) => {
-			const image = img.cloneNode(),
-				text = this.config.texts[ index ];
+    // initializing roullette from fetched images
+    _initRoullette() {
+        this.config.images.forEach(( img, index ) => {
+            const image = img.cloneNode(),
+                text = this.config.texts[ index ];
 
-			// Reset image element to lightbox css classes
-			image.classList.remove( ...image.classList );
-			image.classList.add( "roullette-image", this.control );
+            // Reset image element to lightbox css classes
+            image.classList.remove( ...image.classList );
+            image.classList.add( "roullette-image", this.control );
 
-			// Pushing images to roullete
-			this.config.roullette.img.appendChild( image );
-			this.config.roullette.txt.push( text.textContent );
-		});
+            // Pushing images to roullete
+            this.config.roullette.img.appendChild( image );
+            this.config.roullette.txt.push( text.textContent );
+        });
 
-		// Finally sets the lightbox size to length of roullette
-		this.lightboxSize = this.config.roullette.txt.length;
-	}
+        // Finally sets the lightbox size to length of roullette
+        this.lightboxSize = this.config.roullette.txt.length;
+    }
 
-	// SETTERS
-	_setPhoto( src ) { this.config.lightbox.photo.src = src; }
-	_setCaption( text ) { this.config.lightbox.caption.textContent = text; }
+    // SETTERS
+    _setPhoto( src ) { this.config.lightbox.photo.src = src; }
+    _setCaption( text ) { this.config.lightbox.caption.textContent = text; }
 
-	// Update position ( certain positions are conditioned buttons )
-	_setLastPosition( position ) { this.lastPosition = position; }
+    // Update position ( certain positions are conditioned buttons )
+    _setLastPosition( position ) { this.lastPosition = position; }
 
-	_updateFromAll() {
-		const photo = this.config.roullette.images,
-			caption = this.config.roullette.txt;
+    _updateFromAll( position ) {
+        const photo = this.config.roullette.images,
+            caption = this.config.roullette.txt;
 
-		this._setPhoto( photo.position.src );
-		this._setCaption( caption.position );
-	}
+        this._setPhoto( photo[position].src );
+        this._setCaption( caption[position] );
+    }
 
-	// Update from previous button
-	_updateFromPrevious() {
-		const position = this.lastPosition > 0
-			? --this.lastPosition 
-			: this.lightboxSize;
+    // Update from previous button
+    _updateFromPrevious() {
+        const position = this.lastPosition > 0
+            ? --this.lastPosition 
+            : this.lightboxSize;
 
-		this._updateFromAll( position );
+        this._updateFromAll( position );
         
-		return position;
-	}
+        return position;
+    }
 
-	// Update from next button
-	_updateFromNext() {
-		const position = this.lastPosition < this.lightboxSize
-			? ++this.lastPosition 
-			: 0;
+    // Update from next button
+    _updateFromNext() {
+        const position = this.lastPosition < this.lightboxSize
+            ? ++this.lastPosition 
+            : 0;
 
-		this._updateFromAll( position );
+        this._updateFromAll( position );
 
-		return position;
-	}
+        return position;
+    }
 
-	// Update from roullete image
-	_updateFromRoullette( index ) {
-		const length = this.config.conditions.length,
-			position = index - length;
+    // Update from roullete image
+    _updateFromRoullette( index ) {
+        const length = this.config.conditions.length,
+            position = index - length;
 
-		this._updateFromAll( position );
+        this._updateFromAll( position );
 
-		return position;
-	}
+        return position;
+    }
 
-	// Update from grid of images
-	_updateFromImages( index ) {
-		const length = this.config.conditions.length,
-			position = ( index - this.lightboxSize )  - length;
+    // Update from grid of images
+    _updateFromImages( index ) {
+        const length = this.config.conditions.length,
+            position = ( index - this.lightboxSize )  - length;
 
-		this._updateFromAll( position );
+        this._updateFromAll( position );
 
-		return position;
-	}
+        return position;
+    }
 
-	// If conditions return True
-	_validUpdate ( classList ) {
-		const conditions = this.config.conditions;
+    // If conditions return True
+    _validUpdate ( classList ) {
+        const conditions = this.config.conditions;
 
-		return classList.contains( conditions.name );
-	}
+        return classList.contains( conditions.name );
+    }
 
-	// Route depending on position
-	_updateFrom( classList, index ) {
-		let position;
+    // Route depending on position
+    _updateFrom( classList, index ) {
+        let position;
 
-		if ( this._validUpdate( classList, "roullette" )) {
-			position = this._updateFromRoullette( index );
+        if ( this._validUpdate( classList, "roullette" )) {
+            position = this._updateFromRoullette( index );
 
-		} else if ( this._validUpdate( classList, "previous" )) {
-			position = this._updateFromPrevious();
+        } else if ( this._validUpdate( classList, "previous" )) {
+            position = this._updateFromPrevious();
 
-		} else if ( this._validUpdate( classList, "next" )) {
-			position = this._updateFromNext();
+        } else if ( this._validUpdate( classList, "next" )) {
+            position = this._updateFromNext();
 
-		} else { position = this._updateFromImages( index ); }
+        } else { position = this._updateFromImages( index ); }
 
-		this._setLastPosition( position );
-	}
+        this._setLastPosition( position );
+    }
 
-	// Update state from all conditions
-	_update() {
+    // Update state from all conditions
+    _update() {
         
-		const lastClick = this.handler.lastClicked(),
-			classList = lastClick.element.classList;
+        const lastClick = this.handler.lastClicked(),
+            classList = lastClick.element.classList;
             
-		if ( classList.contains( this.config.exit )) return;
+        if ( classList.contains( this.config.exit )) return;
 
-		this._updateFrom( classList, lastClick.index );
-	}
+        this._updateFrom( classList, lastClick.index );
+    }
 
-	// Listener handler
-	async listen() {
-		this.handler.setAfterFunc( this._update, this ); 
-		return this.handler.onClick( this.config.control, this.conditions )
-			.then( console.log( "Lightbox is working!" ));
-	}
+    // Listener handler
+    async listen() {
+        this.handler.setAfterFunc( this._update, this ); 
+        return this.handler.onClick( this.config.control, this.conditions )
+            .then( console.log( "Lightbox is working!" ));
+    }
 }
 
 // Lightbox event handler 
 class LightboxHandler {
 
-	// ... Animation { element: idName, css: className || [className] }
-	constructor( ...animations ) {
-		// Init animations.
-		this.animations = [];
-		this._initAnimations( animations );
-	}
+    // ... Animation { element: idName, css: className || [className] }
+    constructor( ...animations ) {
+        // Init animations.
+        this.animations = [];
+        this._initAnimations( animations );
+    }
 
-	// Setting animations object and css array
-	_initAnimations( animations ) {
-		animations.forEach(( anim, index ) => {
-			this.animations.push({
-				element: document.getElementById( anim.element ),
-				css: Array.isArray( anim.css ) ? anim.css : [ anim.css ]
-			});
+    // Setting animations object and css array
+    _initAnimations( animations ) {
+        animations.forEach(( anim, index ) => {
+            this.animations.push({
+                element: document.getElementById( anim.element ),
+                css: Array.isArray( anim.css ) ? anim.css : [ anim.css ]
+            });
 
-			this._setDefaultAnimation( index );
-		});
-	}
+            this._setDefaultAnimation( index );
+        });
+    }
 
-	// If css provided is an array
-	_setDefaultAnimation( index ) {
-		const animation = this.animations[ index ],
-			element = animation.element,
-			css = animation.css;
+    // If css provided is an array
+    _setDefaultAnimation( index ) {
+        const animation = this.animations[ index ],
+            element = animation.element,
+            css = animation.css;
 
-		if ( css.length > 1 ) this._toggleAnimation( element, css[ 0 ]);
-	}
+        if ( css.length > 1 ) this._toggleAnimation( element, css[ 0 ]);
+    }
 
-	// Toggle class list item
-	_toggleAnimation( element, css ) { element.classList.toggle( css ); }
+    // Toggle class list item
+    _toggleAnimation( element, css ) { element.classList.toggle( css ); }
 
-	// For each animation, animate
-	_animate() {
-		this.animations.forEach(( animation ) => {
-			animation.css.forEach(( cssName ) => {
-				this._toggleAnimation( animation.element, cssName );
-			});
-		});
-	}
+    // For each animation, animate
+    _animate() {
+        this.animations.forEach(( animation ) => {
+            animation.css.forEach(( cssName ) => {
+                this._toggleAnimation( animation.element, cssName );
+            });
+        });
+    }
 
-	// Needs for logic in Lightbox, controls roullette updates
-	setAfterFunc( func, that, ...args ) {
-		this._afterFunc = () => {
-			if ( typeof func == "function"
+    // Needs for logic in Lightbox, controls roullette updates
+    setAfterFunc( func, that, ...args ) {
+        this._afterFunc = () => {
+            if ( typeof func == "function"
                 && typeof that == "object" )
-				return that[ func.name ]( args );
-		};
+                return that[ func.name ]( args );
+        };
 
-		return this;
-	}
+        return this;
+    }
 
-	// Only update animation if its condition returns true
-	_isConditioned( conditions ) {
-		if ( conditions === null ) return false;
+    // Only update animation if its condition returns true
+    _isConditioned( conditions ) {
+        if ( conditions === null ) return false;
             
-		const classList = this.lastClick.element.classList;
-		let isConditioned = false;
+        const classList = this.lastClick.element.classList;
+        let isConditioned = false;
 
-		Object.keys( conditions ).forEach(( c ) => {
-			if ( classList.contains( conditions[ c ])) 
-				isConditioned = true;
-		});
+        Object.keys( conditions ).forEach(( c ) => {
+            if ( classList.contains( conditions[ c ])) 
+                isConditioned = true;
+        });
 
-		return isConditioned;
-	}
+        return isConditioned;
+    }
 
-	// Controls if have conditions
-	_trigger( conditions ) {
-		if ( this._isConditioned( conditions )) return;
+    // Controls if have conditions
+    _trigger( conditions ) {
+        if ( this._isConditioned( conditions )) return;
 
-		this._animate();
-	}
+        this._animate();
+    }
 
-	// Execute all functions atached to the event
-	_execution( conditions = null ) {
-		this._trigger( conditions );
+    // Execute all functions atached to the event
+    _execution( conditions = null ) {
+        this._trigger( conditions );
         
-		if ( typeof this._afterFunc === "function" )
-			this._afterFunc();
-	}
+        if ( typeof this._afterFunc === "function" )
+            this._afterFunc();
+    }
 
-	// Needs in Lightbox class, return last clicked element
-	lastClicked() { return this.lastClick; }
+    // Needs in Lightbox class, return last clicked element
+    lastClicked() { return this.lastClick; }
 
-	// Each click updates lastClick variable 
-	async onClick( controls, conditions ) {
-		const keys = document.querySelectorAll( `.${controls}` );
+    // Each click updates lastClick variable 
+    async onClick( controls, conditions ) {
+        const keys = document.querySelectorAll( `.${controls}` );
 
-		keys.forEach(( e, i ) => {
-			e.addEventListener( "click", () => {
-				this.lastClick = { 
-					"element": e,
-					"index": ( i - 1 ) 
-				};
+        keys.forEach(( e, i ) => {
+            e.addEventListener( "click", () => {
+                this.lastClick = { 
+                    "element": e,
+                    "index": ( i - 1 ) 
+                };
 
-				this._execution( conditions );
-			});
-		});
+                this._execution( conditions );
+            });
+        });
 
-		return this;
-	}
+        return this;
+    }
 }
--- src/main.js
+++ src/main.js
@@ -12,53 +12,53 @@ import "./styles/index.scss";
 
 // Initialize menu event.
 const initMenu = () => {
-	const menu = new Handler({
-		element: "menu",
-		css: [ "hideLeft", "showLeft" ],
-	},
-	{
-		element: "menu-button",
-		css: [ "unrotateRight", "rotateRight" ],
-	});
+    const menu = new Handler({
+        element: "menu",
+        css: [ "hideLeft", "showLeft" ],
+    },
+    {
+        element: "menu-button",
+        css: [ "unrotateRight", "rotateRight" ],
+    });
 
-	return menu.onClick( "menu-control" );
+    return menu.onClick( "menu-control" );
 };
 
 // Initialize document event.
 const initDocument = () => {
-	const document = new Handler({
-		element: "document",
-		css: [ "disappear", "appear" ]
-	},
-	{
-		element: "loader",
-		css: "disappear"
-	});
+    const document = new Handler({
+        element: "document",
+        css: [ "disappear", "appear" ]
+    },
+    {
+        element: "loader",
+        css: "disappear"
+    });
 
-	return document.onTimeout( 900 );
+    return document.onTimeout( 900 );
 };
 
 // Main function.
 window.addEventListener( "load", () => {
-	initMenu()
-		.then( initDocument())
-		.then( new Lightbox({
+    initMenu()
+        .then( initDocument())
+        .then( new Lightbox({
 
-			images: "grid-image",
-			texts: "grid-caption",
-			css: [ "disappear", "appear" ]
+            images: "grid-image",
+            texts: "grid-caption",
+            css: [ "disappear", "appear" ]
 
-		})).then( console.log( "Is working..." ));
+        })).then( console.log( "Is working..." ));
 });
 
 // Typed.js
 new Typed( "#typed", {
-	strings: [ "d", "designers.", "d", "developers.", "", "you!" ],
-	typeSpeed: 90,
-	backSpeed: 50,
-	startDelay: 2000,
-	backDelay: 1000,
-	loop: true
+    strings: [ "d", "designers.", "d", "developers.", "", "you!" ],
+    typeSpeed: 90,
+    backSpeed: 50,
+    startDelay: 2000,
+    backDelay: 1000,
+    loop: true
 });
 
 
